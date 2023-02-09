8131ad20dd38b417bb64ceaced076e83716e3f18
Jesus S <jsalazar2010@gmail.com>
2023-02-08 17:04:57 -0700

    add vars for current weather section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-/* Psuedo code starts here  */
-
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -15,57 +13,57 @@ var apiKey = "311ffd901dcf798ae2fbc6d7d4ce62de";
 var searchInput = document.querySelector("#search-input");
 var submitBtn = document.querySelector("#submit-btn");
 
-var cityBtn = document.querySelector("#city-btn"); 
+var cityBtn = document.querySelector("#city-btn");
 var cityName = document.querySelector(".city-name");
 var dateEl = document.querySelector('.date');
-var condEl =document.querySelector('.cond');
+var condEl = document.querySelector('.cond');
 var tempEl = document.querySelector('.temp');
 var windEl = document.querySelector('.wind');
 var humidEl = document.querySelector('.humid');
 
-var forecast1 = document.querySelector('#forecast1');
-var forecast2 = document.querySelector('#forecast2');
-var forecast3 = document.querySelector('#forecast3');
-var forecast4 = document.querySelector('#forecast4');
-var forecast5 = document.querySelector('#forecast5');
-
 var forecast = document.querySelector('.forecast');
 
-function renderCurrent (weather) {
-  // console.log(weather);
-
+// push text info to display div 
+function renderCurrent(weather) {
+  console.log(weather);
   cityName.textContent = weather.city.name;
-  tempEl.textContent = `temp: ${weather.list[0].main.temp}`;
-}
+  dateEl.textContent = `${weather.list[0].dt_txt}`;
 
-function renderForecast (weather) {
+  condEl.setAttribute("src", `http://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}.png`);
+  tempEl.textContent = `Temp: ${weather.list[0].main.temp}°F`;
+  windEl.textContent = `Wind: ${weather.list[0].wind.speed}MPH`;
+  humidEl.textContent = `Humidity: ${weather.list[0].main.humidity}%`;
+}
+// get info for 5-day forecast 
+function renderForecast(weather) {
   forecast.textContent = "";
 
-  for (let index = 0; index < weather.list.length; index=index+8) {
+  for (let index = 0; index < weather.list.length; index = index + 8) {
     //const element = array[index];
     var date = new Date(weather.list[index].dt * 1000);
     var formattedDate = date.toLocaleDateString();
     console.log(weather.list[index]);
-    
+
     var card = document.createElement("div");
     var dateCard = document.createElement("p");
     var icon = document.createElement("img");
 
     card.setAttribute("class", "days");
     dateCard.textContent = formattedDate;
-    icon.setAttribute("src", `http://openweathermap.org/img/wn/${weather.list[index].weather[0].icon}.png`)
+    icon.setAttribute("src", `http://openweathermap.org/img/wn/${weather.list[index].weather[0].icon}.png`);
     forecast.appendChild(card);
     card.appendChild(dateCard);
     dateCard.append(icon);
   }
 }
-
+// save cities to local storage, display list of cities 
 function saveCity() {
   var cityHistory = JSON.parse(localStorage.getItem("history")) || [];
   cityHistory.push(searchInput.value);
   localStorage.setItem("history", JSON.stringify(cityHistory));
-}
 
+}
+// start Fetch API request 
 function startApi() {
   var getGeocode = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&units=imperial&appid=${apiKey}`;
 
@@ -74,7 +72,6 @@ function startApi() {
       return response.json();
     })
     .then(function (geodata) {
-      
 
       renderCurrent(geodata);
       renderForecast(geodata);
@@ -83,54 +80,33 @@ function startApi() {
     // do we want a catch error method?
     .catch(err => console.error(err)
     );
-  // function for local storage 
-  //localStorage.setItem();
-  // if cities are saved, display list of cities 
 }
 
 // event listener goes here for click 
 submitBtn.addEventListener("click", function (event) {
   // prevent default for form input 
   event.preventDefault();
+  // require input in search
+  if (!submitBtn) {
+    alert('You need a search input value!');
+    return;
+  }
+  // create button to display list of cities in local storage 
+  var createBtn = document.createElement("button");
+  createBtn.setAttribute("class", "city-btn");
+  var BtnList = document.querySelector("#btn-list");
+  BtnList.appendChild(createBtn);
+  createBtn.textContent = localStorage.getItem(history);
+
   startApi()
 
 });
 
 
 
-
-
-
-// API for GeoCoding URL 
-// can this be template literal??
-
-
 // add event listener to create buttons element?
 var newBtn = document.createElement('button');
-
-// add city text into buttons - from local storage
-//cityBtn.textContent = ;
 
 // populate info for each 5-day forecast 
 var addForecastInfo = document.createElement('p');
 
-
-/* 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-*/
